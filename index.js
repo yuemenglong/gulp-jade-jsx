bd9c5ae03fb7d09334c54a4915fa7426d72ced07
yml <yuemenglong@126.com>
2016-05-10 17:42:08 +0800

    publish 1.0.0

---
+++ index.js
@@ -0,0 +1,20 @@
+var gutil = require('gulp-util');
+var through = require('through2');
+var jadeToJsx = require("jade-jsx");
+
+module.exports = function(options) {
+    return through.obj(function(file, enc, cb) {
+        if (file.isNull()) {
+            this.push(file);
+            return cb();
+        }
+        if (file.isStream()) {
+            this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+            return cb();
+        }
+        var content = jadeToJsx(file.contents.toString());
+        file.contents = new Buffer(content);
+        this.push(file);
+        return cb();
+    });
+};
