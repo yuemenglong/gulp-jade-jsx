532791b415bf333252d8cdfd032383440f1ae26f
yml <yuemenglong@126.com>
2016-05-14 10:39:55 +0800

    use jade-jsx template edition

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var jadeToJsx = require("jade-jsx");
 
-module.exports = function(options) {
+module.exports = function(fn) {
     return through.obj(function(file, enc, cb) {
         if (file.isNull()) {
             this.push(file);
@@ -12,7 +12,7 @@ module.exports = function(options) {
             this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
             return cb();
         }
-        var content = jadeToJsx(file.contents.toString());
+        var content = jadeToJsx(file.contents.toString(), fn);
         file.contents = new Buffer(content);
         this.push(file);
         return cb();
