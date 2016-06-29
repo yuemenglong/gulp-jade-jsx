ab6b77b312b4741163f4089c8bff00f3691ac619
yml <yuemenglong@126.com>
2016-06-29 14:20:12 +0800

    add error log to spec which file fail

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var gutil = require('gulp-util');
 var through = require('through2');
 var jadeToJsx = require("jade-jsx");
+var util = require("util");
+var _ = require("lodash");
 
 module.exports = function(options) {
     return through.obj(function(file, enc, cb) {
@@ -12,9 +14,19 @@ module.exports = function(options) {
             this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
             return cb();
         }
-        var content = jadeToJsx(file.contents.toString());
-        file.contents = new Buffer(content);
-        this.push(file);
-        return cb();
+        try {
+            var content = jadeToJsx(file.contents.toString());
+            file.contents = new Buffer(content);
+            this.push(file);
+            return cb();
+        } catch (ex) {
+            var info = util.format("|JADE_JSX_ERROR, [%s]|", file.path);
+            var line = "+" + _.times(info.length - 1, _.noop).join("-") + "+";
+            console.error(line);
+            console.error(info);
+            console.error(line);
+            throw ex;
+        }
+
     });
 };
